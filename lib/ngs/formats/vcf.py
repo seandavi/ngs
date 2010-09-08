aee54539b2a7b83bea7ae247991225a8404e5524
Sean Davis <seandavi@gmail.com>
2010-09-08 15:58:51 -0400

    Cleaned up vcf.py so that at least it works to return region-like objects.

--- lib/ngs/formats/vcf.py
+++ lib/ngs/formats/vcf.py
@@ -1,34 +1,47 @@
+"""
+A module for opening and parsing a VCF format file and encapsulating VCF records
+
+The Variant Call Format (VCF) is described `here <http://1000genomes.org/wiki/doku.php?id=1000_genomes:analysis:vcf4.0>`
+"""
+
+__docformat__ = 'restructuredtext'
+
 import gzip
 
+
 class vcfFile:
-    nextLine=None
     
     def __init__(self,fname):
         self.fname=fname
-        self.fh=None
-        if(fname.endswith('.gz')):
-            self.fh=gzip.open(fname,'r')
+        self.open()
+
+    def parse(self):
+        for line in self.fh:
+            (yield vcfRecord(line.strip()))
+
+    def close(self):
+        self.is_open=False
+        self.fh.close()
+
+    def open(self):
+        if(self.fname.endswith('.gz')):
+            self.fh=gzip.open(self.fname,'r')
         else:
-            self.fh=open(fname,'r')
+            self.fh=open(self.fname,'r')
+        self.is_open=True
         self.header=[]
         line=self.fh.readline()
         while(line.startswith("#")):
-            self.header.add(line.strip())
-            line=self.fh.readline()
-        self.nextline=line
-
-    def parse(self):
-        if(self.nextLine is not None):
-            yield vcfRecord(self.nextLine)
-            self.nextLine=self.fh.readline().strip()
+            self.header.append(line.strip())
+            line=self.fh.readline()        
 
 class vcfRecord:
     def __init__(self,line):
         parts=line.split()
         self.chromosome=parts[0]
-        self.position=parts[1]
-        self.start=self.position-1
-        self.end=self.position
+        self.position=int(parts[1])
+        self.rbeg=self.position-1
+        self.rend=self.position
         self.name=parts[2]
         self.ref=parts[3]
         self.alt=parts[4]
@@ -38,9 +51,3 @@ class vcfRecord:
         self.form=parts[8]
         self.samples=parts[9:]
 
-f = gzip.open("/import/solexa1/sdavis/sequencing/NCI60/gatkVCF/2_42YM7AAXX.118_BUSTARD-2010-04-21.vcf4.gz",'r')
-j=0
-for i in f:
-    if(not (i.startswith('#'))):
-        v1 = vcfRecord(i.strip())
-        j=j+1
