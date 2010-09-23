793e396584addde8a5044f400a19e84133f6d47e
Sean Davis <seandavi@gmail.com>
2010-09-23 18:36:28 -0400

    moved lib to src

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name="NGS",
     version="0.1",
-    packages=find_packages('lib'),
-    package_dir = {'':'lib'},
+    packages=find_packages('src'),
+    package_dir = {'':'src'},
     scripts = ['scripts/overlapVCF.py','scripts/qseq2fastq.py'],
     )
