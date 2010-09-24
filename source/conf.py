81ce9a8292c89afc91fbad4705818751e8432c87
Sean Davis <seandavi@gmail.com>
2010-09-24 05:16:31 -0400

    Turned on todo marking in documentation

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,9 @@ sys.path.append(os.path.abspath('.'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage']
 
+# turn on/off todos
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
