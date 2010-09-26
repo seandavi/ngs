d30c0694eb1d26f11043f53dac6d645380a10430
Sean Davis <seandavi@gmail.com>
2010-09-26 17:58:44 -0400

    Added fnameutils.py
    Adds a function to parse standard solexadb filenames into component
    parts.

---
+++ source/ngs/solexadb/fnameutils.py
@@ -0,0 +1,27 @@
+import re
+
+BNAME_WITH_READNO_RE = re.compile("(?P<lane>[1-8])_(?P<read>[12,]*)_(?P<flowcell>\w+)\.(?P<id>[^\.]+)(?P<extension>.*)")
+BNAME_WITHOUT_READNO_RE = re.compile("(?P<lane>[1-8])_(?P<flowcell>\w+)\.(?P<id>[^\.]+)(?P<extension>.*)")
+
+def parseFname(fname,withReadNumber=False):
+    """
+    Parse a standard sequence/export/summary filename into parts
+
+    :param fname: String filename from from the solexa database
+    :param withReadNumber: Boolean indicating whether or not the read number is included in the filename
+    :rtype: dict with several keys including lane, flowcell, id, extension ('' if none), and when withReadNumber=True, read
+
+    >>> import ngs.solexadb.fnameutils as fnutils
+    >>> fnutils.parseFname("1_1,2_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True)
+    {'read': '1,2', 'lane': '1', 'flowcell': '622RWAAXX', 'id': '154_BUSTARD-2010-08-31', 'extension': '.tgz'}
+    >>> fnutils.parseFname("1_1_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True)
+    {'read': '1', 'lane': '1', 'flowcell': '622RWAAXX', 'id': '154_BUSTARD-2010-08-31', 'extension': '.tgz'}
+    >>> fnutils.parseFname("1_622RWAAXX.154_BUSTARD-2010-08-31.bam")
+    {'lane': '1', 'flowcell': '622RWAAXX', 'id': '154_BUSTARD-2010-08-31', 'extension': '.bam'}
+    """
+    if(withReadNumber):
+        return(BNAME_WITH_READNO_RE.match(fname).groupdict())
+    return(BNAME_WITHOUT_READNO_RE.match(fname).groupdict())
+
+    
+    
--- source/solexadb.rst
+++ source/solexadb.rst
@@ -1,5 +1,15 @@
 Working with the SolexaDB
 =========================
+Our solexa database is highly tailored to our work environment, so it is probably not useful outside of our lab, but the concepts in it are pretty standard.  In any case, I need a few utilities for working with local data, so this is where they are stashed.
+
+Interacting with the Database Directly
+--------------------------------------
 .. automodule:: ngs.solexadb.model
+   :members:
+   :undoc-members:
+
+Filename Utilities for Standard Filenames
+-----------------------------------------
+.. automodule:: ngs.solexadb.fnameutils
    :members:
    :undoc-members:
