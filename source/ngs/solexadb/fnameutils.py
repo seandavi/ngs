d891177eb6d103dfbc9bb8a9a277fff27a32481f
Sean Davis <seandavi@gmail.com>
2010-10-05 08:41:15 -0400

    added ability to specify read number return with a filname in fnameutils

--- source/ngs/solexadb/fnameutils.py
+++ source/ngs/solexadb/fnameutils.py
@@ -23,25 +23,26 @@ def parseFname(fname,withReadNumber=False):
         return(BNAME_WITH_READNO_RE.match(fname).groupdict())
     return(BNAME_WITHOUT_READNO_RE.match(fname).groupdict())
 
-def getBasename(fname,withReadNumber=False):
+def getBasename(fname,withReadNumber=False,retWithReadNumber=False):
     """
     Parse a standard sequence/export/summary filename into parts
 
     :param fname: String filename from from the solexa database
     :param withReadNumber: Boolean indicating whether or not the read number is included in the filename
+    :param retWithReadNumber: Boolean indicating whether or not to return the read number with the basename
     :rtype: dict with several keys including lane, flowcell, id, extension ('' if none), and when withReadNumber=True, read
 
     >>> import ngs.solexadb.fnameutils as fnutils
-    >>> fnutils.getBasename("1_1,2_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True)
+    >>> fnutils.getBasename("1_1,2_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True,retWithReadNumber=True)
     '1_1,2_622RWAAXX.154_BUSTARD-2010-08-31'
-    >>> fnutils.getBasename("1_1_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True)
+    >>> fnutils.getBasename("1_1_622RWAAXX.154_BUSTARD-2010-08-31.tgz",withReadNumber=True,retWithReadNumber=True)
     '1_1_622RWAAXX.154_BUSTARD-2010-08-31'
     >>> fnutils.getBasename("1_622RWAAXX.154_BUSTARD-2010-08-31.bam")
     '1_622RWAAXX.154_BUSTARD-2010-08-31'
     """    
 
     retdict = parseFname(fname,withReadNumber)
-    if(withReadNumber):
+    if(retWithReadNumber):
         return("%(lane)s_%(read)s_%(flowcell)s.%(id)s" % (retdict))
     else:
         return("%(lane)s_%(flowcell)s.%(id)s" % (retdict))
