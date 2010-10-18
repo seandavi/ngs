367f957227f326ecd1d0f26462b2b467ba2e1202
Sean Davis <seandavi@gmail.com>
2010-10-18 15:35:32 -0400

    Planning on using sample sheets instead of the database

--- source/ngs/solexadb/model.py
+++ source/ngs/solexadb/model.py
@@ -56,5 +56,38 @@ class SolexaDB:
         :rtype: An object of type :class:`sqlalchemy.orm.Session`
         """
         return(self.Session())
+
+    def getSampleSheetByStudyID(self,study_id):
+        """
+        Get the sample sheet from the database for a given Study ID.
+
+        :param study_id: Study id to match
+        :type study_id: Int
+        :rtype: dict('keys':column names from database,'rows':The actual rows for the records)
+        """
+        con = self.engine.connect()
+        res = con.execute("""
+        select fcia.flowcellName,sr.date,r1.lane,nread,read1,read2,index_read,sequenceFile1,sequenceFile2,sequenceFile3,ssource.name as source_name,ssource.id as source_id,samp.name as sample_name,samp.id as sample_id,samp.variable as sample_variables,slib.name as library_name,slib.id as library_id, solexa_study.id as study_id, solexa_study.study_title from soldb.flowCellBasecallLane fcbcl
+join (select basecallFile as sequenceFile1,lane,basecallId from soldb.flowCellBasecallLane where readnumber=0) as r1 on r1.basecallId=fcbcl.basecallId and fcbcl.lane=r1.lane and fcbcl.readnumber=0
+left outer join (select basecallFile as sequenceFile2,lane,basecallId from soldb.flowCellBasecallLane where readnumber=1) as r2 on r2.basecallId=fcbcl.basecallId and r2.lane=r1.lane
+left outer join (select basecallFile as sequenceFile3,lane,basecallId from soldb.flowCellBasecallLane where readnumber=2) as r3 on r3.basecallId=fcbcl.basecallId and r3.lane=r2.lane
+join flowCellBasecall fcbc on fcbcl.basecallId=fcbc.id
+join solexa.uniqueByMaxDateFlowCellBaseCallLane u on fcbc.id=u.basecallid and fcbcl.lane=u.lane
+join flowCellImageAnalysis fcia on fcbc.imageAnalysisId=fcia.id
+join solexa_run sr on sr.flowcell=fcia.flowcellName
+join solexa_lane sl on sr.id=sl.run_id and sl.lane=fcbcl.lane
+join solexa_library slib on slib.id=sl.library_id
+join solexa_sample_library samplib on samplib.library_id=slib.id
+join solexa_sample samp on samp.id=samplib.sample_id
+join solexa_source_sample soursamp on soursamp.sample_id=samp.id
+join solexa_source ssource on ssource.id=soursamp.source_id
+join solexa_study_sample on solexa_study_sample.sample_id=samp.id
+join solexa_study on solexa_study_sample.study_id=solexa_study.id
+where study_id=%d
+""" % (study_id))
+        keys = res.keys()
+        return({'keys':res.keys(),'rows':res.fetchall()})
+
+
     
 
