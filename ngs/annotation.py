2214b82c039dd0cce7ca1b1670ed3565e077932e
Sean Davis <seandavi@gmail.com>
2010-11-09 08:54:20 -0500

    Working on rich transcript functionality--not correct just yet

--- ngs/annotation.py
+++ ngs/annotation.py
@@ -10,6 +10,47 @@ class Transcript(object):
             raise Exception("exonStarts and exonEnds are not the same size")
         self.exons=zip(sorted(exonStarts),sorted(exonEnds))
 
+    def exons(self):
+        if(self.strand=='+'):
+            return(self.exons.sort())
+        else:
+            return(self.exons.reverse())
+
+    def cdsExons(self):
+        retexons = []
+        if(self.strand=='+'):
+            for exon in sorted(self.exons):
+                # full exon is CDS
+                if((exon[0]>self.cdsStart) &
+                   (exon[1]<=self.cdsEnd)):
+                    retexons.append(exon)
+                # exon contains 5' UTR
+                if((self.cdsStart>exon[0]) &
+                   (self.cdsStart<exon[1])):
+                    retexons.append((self.cdsStart,exon[1]))
+                # exon contains 3' UTR
+                if((self.cdsEnd>exon[0]) &
+                   (self.cdsEnd<exon[1])):
+                    retexons.append((exon[0],self.cdsEnd))
+            return(retexons)
+        else:
+            for exon in reversed(self.exons):
+                # full exon is SELF.CDS
+                if((exon[0]>self.cdsStart) &
+                   (exon[1]<self.cdsEnd)):
+                    retexons.append(exon)
+                # exon contains 5' UTR
+                if((self.cdsEnd>exon[0]) &
+                   (self.cdsEnd<exon[1])):
+                    retexons.append((exon[0],self.cdsEnd))
+                # exon contains 3' UTR
+                if((self.cdsStart>exon[0]) &
+                   (self.cdsStart<exon[1])):
+                    retexons.append((self.cdsStart,exon[1]))
+            return(retexons)
+            
+        
+
 class TranscriptDB(object):
     def __init__(self,genome='hg18'):
         self.genome=genome
