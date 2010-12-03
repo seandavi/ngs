b6d1d4251d5509eeb873c4b0900104901660c0c3
Sean Richard Davis <sedavis@helix.nih.gov>
2010-12-03 08:42:13 -0500

    Added support for writing file directly to qseq2fastq.py
    This change allows one to check for file existence and exit if it exists,
    for example.

--- scripts/qseq2fastq.py
+++ scripts/qseq2fastq.py
@@ -7,18 +7,20 @@ import os
 import re
 import tarfile
 import ngs.formats.qseq
+import gzip
 
 def phred64ToStdqual(qualin):
     return(''.join([chr(ord(x)-31) for x in qualin]))
 
-def outputFastq(f2,phred33=False):
+def outputFastq(f2,outfile,phred33=False):
+    print(outfile)
     for line in f2:
         sp = line.strip().split("\t")
         qual=sp[9]
         if(phred33):
             qual=ngs.formats.qseq.phred64ToStdqual(sp[9])
         seqString = sp[8].replace(".","N")
-        print "@%s:%s:%s:%s:%s\n%s\n+\n%s" % (sp[0],sp[2],sp[3],sp[4],sp[5],seqString,qual)
+        outfile.write("@%s:%s:%s:%s:%s\n%s\n+\n%s\n" % (sp[0],sp[2],sp[3],sp[4],sp[5],seqString,qual))
 
 usage = "usage: %prog [options] TarFileOrBustardDirectoryName"
 parser = OptionParser(usage=usage)
@@ -29,24 +31,42 @@ parser.add_option("-v", "--verbose", dest="verbose",
 parser.add_option("-r", "--read",type='int',
                   dest="readNumber", default=1,
                   help="Which read to convert (1 or 2)")
-parser.add_option("--phred33", action="store_true", default=False,
+parser.add_option("--phred33", action="store_true", default=True,
                   help="Convert phred scores from phred64 to phred33",dest="phred33")
 parser.add_option("-t", "--tarfile", action="store_true", default=False,
                   help="Input is from a gzipped tarfile like that in the analysis sequence directory",dest="tfile")
+parser.add_option("-o","--outfile",dest="outfile",default=None,
+                  help="Output file name")
+parser.add_option("-w","--overwrite",dest="overwrite",action="store_true",
+                  default=False,help="Overwrite existing file with same name")
 
 
 (options, args) = parser.parse_args()
-matchre = re.compile("s_%d_%d.*qseq\\.txt" % (options.lane,options.readNumber))
-
+matchre=re.compile("s_.*qseq\\.txt")
+if((options.readNumber is not None) & (options.lane is not None)):
+    matchre = re.compile("s_%d_%d.*qseq\\.txt" % (options.lane,options.readNumber))
+elif((options.readNumber is not None)):
+    matchre = re.compile("s_.*_%d.*qseq\\.txt" % (options.readNumber))
+elif((options.lane is not None)):
+    matchre = re.compile("s_%d_.*.*qseq\\.txt" % (options.lane))
+    
+outfile=sys.stdout
+if((os.path.exists(options.outfile)) & (~options.overwrite)):
+    exit(0)
+if(options.outfile is not None):
+    if(options.outfile.endswith('gz')):
+        outfile=gzip.open(options.outfile,'wb')
+    else:
+        outfile=open(options.outfile,'w')
 if(options.tfile):
     tfile = tarfile.open(args[0],"r:gz")
     for f in tfile:
         if(f.isfile()):
             f2 = tfile.extractfile(f.name)
-            outputFastq(f2,options.phred33)
+            outputFastq(f2,outfile,options.phred33)
 else:
     for i in os.listdir(args[0]):
         if(matchre.match(i)!=None):
             f2 = open(i,'r')
-            outputFastq(f2,options.phred33)
+            outputFastq(f2,outfile,options.phred33)
             f2.close()
