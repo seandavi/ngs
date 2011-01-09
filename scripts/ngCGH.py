22c57e7e3453e36c375f79833ffcb47ce74a143c
Sean Richard Davis <sedavis@helix.nih.gov>
2011-01-09 09:17:21 -0500

    cleaned up ngCGH a bit and fixed a couple of bugs

--- scripts/ngCGH.py
+++ scripts/ngCGH.py
@@ -19,12 +19,44 @@ class RegionalCount(ngs.regions.Region):
     def __str__(self):
         return("%s:%d-%d <%d>" % (self.chromosome,self.rbeg,self.rend,self._count))
 
+def doNormalComparisonCGH(opts,args):
+    tfile = pysam.Samfile(args[0],'rb')
+    nfile = pysam.Samfile(opts.normalbam,'rb')
+    lengths=nfile.lengths
+    refnames=nfile.references
+    outfile=sys.stdout
+    if(opts.outfile is not None):
+        outfile=open(opts.outfile,'w')
+    for refidx in xrange(tfile.nreferences):
+        n=0
+        tfileiterator=tfile.fetch(refnames[refidx],0,lengths[refidx])
+        tread=tfileiterator.next()
+        for nread in nfile.fetch(refnames[refidx],0,lengths[refidx]):
+            if(nread.is_duplicate): continue
+            if(n==0):
+                startloc = nread.pos
+            n+=1
+            if(n==1000):
+                j=0
+                while((tread.pos<nread.pos) & (tread.rname==nread.rname)):
+                    tread=tfileiterator.next()
+                    if(tread.is_duplicate): continue
+                    j+=1
+                outfile.write("%s\t%d\t%d\t%d\t%d\n" % (refnames[refidx],
+                                              startloc,
+                                              nread.pos,
+                                              1000,
+                                              j))
+                n=0
+
 def main():
     logging.basicConfig(level=10)
     logger = logging.getLogger('CGH')
     parser = optparse.OptionParser(usage="usage: %prog [options] bamfileName")
     parser.add_option('-w','--windowsize',dest='windowsize',type="int",
-                      help='Windowsize for calculation of copy number')
+                      help='Windowsize (bp) for calculation of copy number')
+    parser.add_option('-n','--normalbam',dest='normalbam',
+                      help='The name of the bamfile for the normal comparison')
     parser.add_option('-o','--outfile',dest='outfile',type="string",
                       help='Output filename, default <stdout>')
     parser.add_option('-l','--loglevel',dest='loglevel',type="int",
@@ -32,7 +64,11 @@ def main():
     (opts,args) = parser.parse_args()
     if(opts.loglevel is not None):
         logger.setLevel(opts.loglevel)
+    if(opts.normalbam is not None):
+        doNormalComparisonCGH(opts,args)
+        exit()
     samfile = pysam.Samfile(args[0],'rb')
+
     lengths = samfile.lengths
     regioncounts = []
     refnames = samfile.references
