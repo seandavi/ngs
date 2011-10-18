be1f6e329b0dc7eb04cacd375fd699ba16d3af58
Sean Davis <seandavi@gmail.com>
2011-10-18 17:43:54 -0400

    Working on adding object behaviors to gsnapParser.py.  Currently, reads and alignments work as expected.  Of course, the details are in the alignment blocks, so this takes the longest time to work through.

--- scripts/gsnapParser.py
+++ scripts/gsnapParser.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import gzip
 import collections
+import re
+
+POSITION_REGEX = re.compile('([+-])(.*):(\d+)..(\d+)')
+FIRSTTAG_REGEX = re.compile('(.*):(\d+)..(.*):(\d+)')
+
 
 def openFile(f):
     if(isinstance(f,file)):
@@ -17,23 +22,58 @@ class GsnapRecord(list):
 
 #GsnapRead = collections.namedtyple('GsnapRead','read type qual readname alignments')
 
+
+
 class GsnapRead(object):
     def __init__(self,line):
-        self.readinfo = line
+        sline = line.strip().split("\t")
+        self.read = sline[0][1:]
+        tmp = sline[1].split(" ")
+        self.naligns = int(tmp[0])
+        self.readtype= tmp[1]
+        self.is_translocation=False
+        if(len(tmp)>2):
+            self.is_translocation=True
+        self.readname = sline[3]
+        self.quality  = sline[2]
         self.alignments = []
     def __repr__(self):
-        return("<GsnapRead with %d alignments>" % (len(self.alignments)))
+        return("<GsnapRead with %d alignments, %s>" % (self.naligns,self.readname))
 
 class GsnapAlignment(object):
     def __init__(self,line):
-        self.info = line
-        self.blocks = [line]
+        self.blocks = [GsnapBlock(line)]
     def __repr__(self):
         return("<GsnapAlignment with %d blocks>" % (len(self.blocks)))
 
 class GsnapBlock(object):
     def __init__(self,line):
-        self.info = line
+        sline = line.strip().split("\t")
+        self.alignstring = sline[0][1:]
+        (self.blockstart,self.blockend) = map(int,sline[1].split(".."))
+        m = POSITION_REGEX.match(sline[2])
+        self.chrom = m.group(2)
+        self.strand = m.group(1)
+        if(self.strand=='+'):
+            self.start  = int(m.group(3))
+            self.end    = int(m.group(4))
+        else:
+            self.end    = int(m.group(4))
+            self.start  = int(m.group(3))
+        tmpdat = sline[3].split(",")
+        m = FIRSTTAG_REGEX.match(tmpdat[0])
+        self.is_start=False
+        self.is_end  =False
+        self.startclip=0
+        self.endclip  =0
+        if(m.group(1)=='start'):
+            self.is_start=True
+            self.startclip = int(m.group(2))
+        if(m.group(3)=='end'):
+            self.is_end  =True
+            self.endclip = int(m.group(4))
+        self.tmpmap = dict([x.split(":") for x in tmpdat[1:]])
+        
 
 class GsnapFile(object):
     def __init__(self,f):
@@ -94,6 +134,9 @@ if __name__ == '__main__':
     j = 0
     for rec in g:
         j+=1
-        print rec
+        for read in rec:
+            for align in read.alignments:
+                for block in align.blocks:
+                    print read.readname,block.blockstart,block.start,block.end,block.is_start,block.is_end,block.tmpmap
 
     print j
